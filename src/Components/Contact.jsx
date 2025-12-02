4c885c6c563619c4674fcab8ab051e119c405c07
mahesh-molugu <molugumahesh345@gmail.com>
2025-12-02 19:40:49 +0530

    updated the css styles

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -33,9 +33,7 @@ const Contact = () => {
   }
   return (
     <motion.div
-      initial={{ opacity: 0, y: 30 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1 }}
+      
     >
       <div className="contact">
        <div className="contact-header">
